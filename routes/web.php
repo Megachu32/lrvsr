eb28106f37c702abd251053b596c7e2c86e30e70
ACE <asaexcelelgib@gmail.com>
2026-01-08 17:07:31 +0700

    layout and views

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,20 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('layouts.app');
-});
-
-Route::get('/home', function () {
     return view('home.index');
 });
+
+Route::get('/community', function () {
+    return view('community.index');
+});
+
+Route::get('/explore', function () {
+    return view('home.explore');
+});
+
+Route::get('/rules', function () {
+    return view('home.rule');
+});
+
+
+
